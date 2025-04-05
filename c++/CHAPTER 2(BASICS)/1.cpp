b0a464d65e09c78d7a1d118d93dd57abda486d8f
sushilsahoo06 <sahoosushil456@gmail.com>
2025-04-06 01:23:23 +0530

    new commit

---
+++ c++/CHAPTER 2(BASICS)/1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"enter a number :";
+  cin>>n;
+  if(n>0){
+    cout<<"n is positive number"<<endl;
+  }else{
+    cout<<"n is negative number"<<endl;
+  }
+}
